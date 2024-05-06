5ef4cb8056947b66b7c9cb1e3b381d4fc77afb1a
Divyajyoti Patra <djpatra07@gmail.com>
2024-05-07 00:51:06 +0530

    Update bitsplitter.v

--- bitsplitter.v
+++ bitsplitter.v
@@ -10,7 +10,6 @@ wire outtff;
 wire held;
 PISO sr(data,clk,ld,outpiso);
 tffp fdff(clk,ld,outtff);
-
-	assign even=odd=(outtff)?1'b0:outpiso;
+assign even=odd=(outtff)?1'b0:outpiso;
 assign odd=(outtff)?outpiso:1'b0;
 endmodule
