cc7fa1cd6d0c41c7e78bcd7e898a01ae300fe0ad
Divyajyoti Patra <djpatra07@gmail.com>
2024-05-07 14:32:03 +0530

    Update bitsplitter.v

--- bitsplitter.v
+++ bitsplitter.v
@@ -3,6 +3,25 @@ module bitsplitter(
 	input clk,
 	input ld,
 	output even,
+	output odd,
+	output [3:0]Sheve,
+	output [3:0]Shodd
+);
+wire outpiso;
+wire outtff;
+wire held;
+PISO sr(data,clk,ld,outpiso);
+tffp fdff(clk,ld,outtff);
+assign even=outtff?1'b0:outpiso;
+assign odd=(outtff)?outpiso:1'b0;
+SIPO sr1(even,odd,clk,outtff,ld,Sheve,Shodd);
+endmodule
+/*
+module bitsplitter(
+	input [7:0]data,
+	input clk,
+	input ld,
+	output even,
 	output odd
 );
 wire outpiso;
@@ -12,3 +31,4 @@ tffp fdff(clk,ld,outtff);
 assign even=odd=(outtff)?1'b0:outpiso;
 assign odd=(outtff)?outpiso:1'b0;
 endmodule
+*/
