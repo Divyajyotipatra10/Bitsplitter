46e6ab522ac0cbd5b4ed8b1f82c91886ee9f98db
Divyajyoti Patra <djpatra07@gmail.com>
2024-05-15 21:45:07 +0530

    Update bitsplitter.v

--- bitsplitter.v
+++ bitsplitter.v
@@ -1,34 +1,17 @@
-module bitsplitter(
-	input [7:0]data,
-	input clk,
-	input ld,
-	output even,
-	output odd,
-	output [3:0]Sheve,
-	output [3:0]Shodd
-);
+module qpskinit(
+    input [7:0]data,
+    input clk,
+    input ld,
+    output [3:0] Sheve,
+    output [3:0] Shodd
+    );
 wire outpiso;
 wire outtff;
-wire held;
-PISO sr(data,clk,ld,outpiso);
-tffp fdff(clk,ld,outtff);
-assign even=outtff?1'b0:outpiso;
-assign odd=(outtff)?outpiso:1'b0;
+wire even;
+wire odd;
+PISO sr(data,clk,ld,outpiso);//NRZ L output
+TFFp fdff(clk,ld,outtff);//T flip flop for deciding between even or odd bits
+assign even=outtff?1'b0:outpiso;//bit splitter
+assign odd=(outtff)?outpiso:1'b0;//bit splitter
 SIPO sr1(even,odd,clk,outtff,ld,Sheve,Shodd);
 endmodule
-/*
-module bitsplitter(
-	input [7:0]data,
-	input clk,
-	input ld,
-	output even,
-	output odd
-);
-wire outpiso;
-wire outtff;
-PISO sr(data,clk,ld,outpiso);
-tffp fdff(clk,ld,outtff);
-assign even=odd=(outtff)?1'b0:outpiso;
-assign odd=(outtff)?outpiso:1'b0;
-endmodule
-*/
